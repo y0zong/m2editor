bcae7260d115af639c382e1cd8a74a5d3961c5b6
Yaozong Wang <yozong.w@outlook.com>
2024-02-03 12:07:32 +0800

    move basic setup to basic

--- src/nuo/inputrule/basic.ts
+++ src/nuo/inputrule/basic.ts
@@ -1,4 +1,13 @@
 import { InputRule, textblockTypeInputRule, wrappingInputRule } from "prosemirror-inputrules";
+import { Schema } from "prosemirror-model"
+import { addListNodes } from "prosemirror-schema-list"
+import * as basic from "prosemirror-schema-basic"
+import { keymap } from "prosemirror-keymap"
+import { dropCursor } from "prosemirror-dropcursor"
+import { gapCursor } from "prosemirror-gapcursor"
+import { history } from "prosemirror-history"
+import { baseKeymap } from "prosemirror-commands"
+import { inputRules, smartQuotes, emDash, ellipsis } from "prosemirror-inputrules"
 import { NodeType } from "prosemirror-model";
 
 export function imageRule(nodeType: NodeType) {
@@ -43,4 +52,31 @@ export function codeBlockRule(nodeType: NodeType) {
 export function headingRule(nodeType: NodeType, maxLevel: number) {
     return textblockTypeInputRule(new RegExp("^(#{1," + maxLevel + "})\\s$"),
         nodeType, match => ({ level: match[1].length }))
+}
+
+// note: basic setup
+const schema = new Schema({
+    nodes: addListNodes(basic.schema.spec.nodes, "paragraph block*", "block"),
+    marks: basic.schema.spec.marks
+})
+
+const rules = smartQuotes.concat(ellipsis, emDash)
+if (schema.nodes.blockquote) rules.push(blockQuoteRule(schema.nodes.blockquote))
+if (schema.nodes.ordered_list) rules.push(orderedListRule(schema.nodes.ordered_list))
+if (schema.nodes.bullet_list) rules.push(bulletListRule(schema.nodes.bullet_list))
+if (schema.nodes.code_block) rules.push(codeBlockRule(schema.nodes.code_block))
+if (schema.nodes.image) rules.push(imageRule(schema.nodes.image))
+if (schema.nodes.heading) rules.push(headingRule(schema.nodes.heading, 6))
+
+const plugins = [
+    inputRules({ rules }),
+    // keymap(buildKeymap(schema, mapKeys)),
+    keymap(baseKeymap),
+    dropCursor(),
+    gapCursor(),
+    history()
+]
+
+export const basicConfig = {
+    schema, plugins
 }
